c62d3daadb20f2ed3f4f92927a14fe954b05c051
Sagarsl51 <sagar895187@gmail.com>
2025-11-30 09:52:15 +0530

    floder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Sagar S L',
+    author_email= 'sagar8951873@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
