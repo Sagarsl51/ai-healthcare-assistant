2465784efc82508f95d5239d1be52633bdd243a1
Sagarsl51 <sagar895187@gmail.com>
2025-12-09 19:54:49 +0530

    intial commit

---
+++ src/prompt.py
@@ -0,0 +1,12 @@
+
+
+
+system_prompt = (
+    "you are an assistant for question-answering tasks. "
+    "use the following pieces of retrieved context to answer"
+    "the question. if you don't know the answer ,say that you"
+    "don't know.use three sentences maximum and keep the "
+    "answer concise."
+    "\n\n"
+    "{context}"
+)
---
+++ store_index.py
@@ -0,0 +1,38 @@
+from src.helper import load_pdf, text_split, download_hugging_face_embeddings
+from pinecone import Pinecone, ServerlessSpec
+from pinecone import Pinecone, ServerlessSpec
+from  pinecone import ServerlessSpec
+from dotenv import load_dotenv
+from langchain_pinecone import PineconeVectorStore
+import os
+
+
+load_dotenv()
+
+PINECONE_API_KEY =os.environ.get('PINECONE_API_KEY')
+os.environ["PINECONE_API_KEY"]=PINECONE_API_KEY
+
+extracted_data=load_pdf('data')
+text_chunks= text_split(extracted_data)
+embeddings = download_hugging_face_embeddings()
+
+pc=Pinecone(api_key=PINECONE_API_KEY)
+
+index_name ="chatbot"
+ 
+pc.create_index(
+    name=index_name,
+    dimension=384,
+    metric="cosine",
+    spec=ServerlessSpec(
+        cloud="aws",
+        region="us-east-1"
+    )
+
+ )
+
+docsearch = PineconeVectorStore.from_documents(
+    documents=text_chunks,
+    index_name=index_name,
+    embedding=embeddings,
+)
